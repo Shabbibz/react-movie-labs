8007c616e0923014b87585cd1ee0d8b4290a9466
Shannon <xXSHANNONTXx@hotmail.com>
2025-02-09 19:21:05 +0000

    Added filtering of movie list - Movie lab 1 complete!

--- movies/src/components/filterMoviesCard/index.jsx
+++ movies/src/components/filterMoviesCard/index.jsx
@@ -41,16 +41,15 @@ export default function FilterMoviesCard(props) {
 
     const handleChange = (e, type, value) => {
         e.preventDefault()
-        // Completed later
-    };
-    const handleTextChange = e => {
-        handleChange(e, "name", e.target.value)
-    }
-    const handleChange = (e, type, value) => {
-        e.preventDefault()
         props.onUserInput(type, value)   // NEW
       }
     
+    const handleTextChange = e => {
+        handleChange(e, "name", e.target.value)
+    }
+    const handleGenreChange = e => {
+        handleChange(e, "genre", e.target.value)
+    };
 
     return (
         <Card
