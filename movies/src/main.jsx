a931d2859cd69bf0393978c92fe416725f99bbc2
Shannon <xXSHANNONTXx@hotmail.com>
2025-02-26 10:26:28 +0000

    Change made

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -5,12 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 
 
-const images = [
-  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-]
+
 
 const App = () => {
   return (
@@ -18,9 +13,12 @@ const App = () => {
       );
 };
 
-
-
-
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
