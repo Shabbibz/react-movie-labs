3656aceeff2047ea61d6eed815ea0c927f8c5537
Shannon <xXSHANNONTXx@hotmail.com>
2025-02-26 16:05:37 +0000

    Fix backward/forward arrows2.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router";
+import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
@@ -11,25 +11,16 @@ import SiteHeader from './components/siteHeader'
 const App = () => {
   return (
     <BrowserRouter>
-          <BrowserRouter>
-      <SiteHeader />
-      <Routes>
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-      </Routes>
-    </BrowserRouter>
+    <SiteHeader />
+    <Routes>
+      <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+      <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+      <Route path="/movies/:id" element={<MoviePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={ <Navigate to="/" /> } />
+    </Routes>
+  </BrowserRouter>
 
-      <Routes>
-        <Route exact path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-      </Routes>
-    </BrowserRouter>
   );
 };
 
