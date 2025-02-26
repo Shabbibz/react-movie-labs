0930c60342241d90d6604479c5b067500c115186
Shannon <xXSHANNONTXx@hotmail.com>
2025-02-26 15:23:30 +0000

    Movie Details Page rendering.

--- movies/src/pages/movieDetailsPage.jsx
+++ movies/src/pages/movieDetailsPage.jsx
@@ -4,10 +4,41 @@ import MovieDetails from "../components/movieDetails/";
 import Grid from "@mui/material/Grid2";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router';
+
 
 const MoviePage = (props) => {
-  const movie = props.movie;
-  const images = props.images;
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((movie) => {
+        // console.log(movie)
+        setMovie(movie);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters)
+      .then((images) => {
+        // console.log(images)
+        setImages(images);
+      });
+    // eslint-disable-next-line
+  }, []);
+
 
   return (
     <>
@@ -15,7 +46,7 @@ const MoviePage = (props) => {
         <>
           <MovieHeader movie={movie} />
           <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid size={{xs: 3}}>
+            <Grid size={{ xs: 3 }}>
               <div sx={{
                 display: "flex",
                 flexWrap: "wrap",
@@ -29,19 +60,20 @@ const MoviePage = (props) => {
                 >
                   {images.map((image) => (
                     <ImageListItem
-                      key={image}
+                      key={image.file_path}
                       cols={1}
                     >
                       <img
-                        src={`https://image.tmdb.org/t/p/w500/${image}`}
-                        alt={image}
+                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                        alt={image.file_path}
                       />
                     </ImageListItem>
                   ))}
                 </ImageList>
+
               </div>
             </Grid>
-            <Grid size={{xs: 9}}>
+            <Grid size={{ xs: 9 }}>
               <MovieDetails movie={movie} />
             </Grid>
           </Grid>
